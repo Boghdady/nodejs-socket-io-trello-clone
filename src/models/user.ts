e4a93fc9f1d7d60fd84bbc18e5dcff0073a22ed2
boghdady <boghdady107@gmail.com>
2022-08-03 12:34:08 +0200

    create user model, user type, hash password and validate password

---
+++ src/models/user.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
+import bcryptjs from 'bcryptjs';
+
+import { UserDocument } from '../types/user.interface';
+
+const userSchema = new Schema<UserDocument>(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      validate: [validator.isEmail, 'invalid email format'],
+      createIndexes: { unique: true },
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcryptjs.genSalt(12);
+    this.password = await bcryptjs.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err as Error);
+  }
+});
+
+userSchema.methods.validatePassword = async function (
+  password: string
+): Promise<Boolean> {
+  return await bcryptjs.compare(password, this.password);
+};
+
+export default model<UserDocument>('User', userSchema);
---
+++ src/types/user.interface.ts
@@ -0,0 +1,14 @@
+import { Document } from 'mongoose';
+
+// This interface will use everywhere
+export interface User {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+// This interface will use in user schema
+export interface UserDocument extends User, Document {
+  validatePassword(pass: string): Promise<Boolean>;
+}
