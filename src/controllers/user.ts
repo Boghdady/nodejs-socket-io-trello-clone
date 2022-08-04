755742b795814319d5591229a776c11b0ba49c61
boghdady <boghdady107@gmail.com>
2022-08-04 15:00:37 +0200

    login logic

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -5,6 +5,7 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/user';
 import { UserDocument } from '../types/user.interface';
 import { JWT_SECRET } from '../config';
+import ApiError from '../utils/apiError';
 
 const normalizeUser = (user: UserDocument) => {
   const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET);
@@ -23,5 +24,19 @@ export const register = asyncHandler(
 
     const savedUser = await user.save();
     res.send(normalizeUser(savedUser));
+  }
+);
+
+export const login = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select('+password');
+    console.log(user);
+
+    if (!user || !(await user.validatePassword(password))) {
+      return next(new ApiError('Invalid email or password', 400));
+    }
+
+    res.status(200).send(normalizeUser(user));
   }
 );
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { register } from '../controllers/user';
+import { login, register } from '../controllers/user';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 export default router;
